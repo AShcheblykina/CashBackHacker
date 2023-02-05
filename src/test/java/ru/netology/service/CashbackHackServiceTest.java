b9c48ac223e6291d39664f7f143e117c63095493
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-02-05 23:02:43 +0300

    create TestNg

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void notUpToTheBonus() {
+        int amount = 900;
+        int expected = 100;
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    public void notUpTheBonus() {
+        int amount = 100;
+        int expected = 900;
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void LimitBonus() {
+        int amount = 1_000;
+        int expected = 1000;
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void notUpBonus() {
+        int amount = 500;
+        int expected = 500;
+
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+}
