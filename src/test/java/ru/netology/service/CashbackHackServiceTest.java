57194429ca5b1782df84af4cd0010b5634958cce
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-02-07 20:48:11 +0300

    create junit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void buyMoreMinToBonus() {
+        int amount = 900;
+        int expected = 100;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void buyMoreMaxToBonus() {
+        int amount = 100;
+        int expected = 900;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void buyMoreHalfToBonus() {
+        int amount = 500;
+        int expected = 500;
+
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+}
